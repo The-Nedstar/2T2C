daad2bba1ed9d2a667725dbc4231fd88654cbb02
The_Tedstar <edward.aplin@st-annes.ox.ac.uk>
2025-07-24 09:57:09 +0100

    making error messages cleaner

--- Functions/Functions for 2T2C.R
+++ Functions/Functions for 2T2C.R
@@ -16,43 +16,42 @@ DataWrangling <- function(FileName, Input, TimeInterval, SensorNames, ROINames){
   ## adding user specified data
   # adding time data
   if (TimeInterval == FALSE){
-    cat("No time interval data presented.", "\n", "\n")
+    cat("No time interval data presented.", "\n")
   }
   else{
     if (data.class(TimeInterval) != "numeric"){
-      cat("ERROR: Time interval data not added as not numeric, please write a number without speechmarks, or FALSE if you don't want to add time interval data.", "\n", "\n")
+      cat("ERROR: Time interval data not added as not numeric, please write a number without speechmarks, or FALSE if you don't want to add time interval data.", "\n")
     }
     else{
       Output$Time <- (Output$Frame-1) * TimeInterval
-      cat("Time interval data added successfully!", "\n", "\n")
+      cat("Time interval data added successfully!", "\n")
     }
   }
   # adding sensor names
   if (all(SensorNames == FALSE)){
-    cat("No sensor name data presented.", "\n", "\n")
+    cat("No sensor name data presented.", "\n")
   }
   else{
     if (length(SensorNames) != max(Output$Channel)){
-      cat('ERROR: Sensor name data entered incorrectly, ensure it is done in the form c("name for sensor 1", "name for sensor 2", ...) and that you have put a name for each sensor present. \n')
-      cat("you have", max(Output$Channel), "Channels that need names", "\n", "\n")
+      message('ERROR: Sensor name data entered incorrectly, ensure it is done in the form c("name for sensor 1", "name for sensor 2", ...) and that you have put a name for each sensor present. \n')
+      message("you entered names for ", length(SensorNames), " channels but need names for ", max(Output$Channel), " channels.", "\n")
     }
     else{
       Output$SensorName <- SensorNames[Output$Channel]
-      cat("Sensor name data added successfully!", "\n", "\n")
+      cat("Sensor name data added successfully!", "\n")
     }
   }
   # adding ROI names
   if (all(ROINames == FALSE)){
-    cat("No ROI name data presented.", "\n", "\n")
+    cat("No ROI name data presented.", "\n")
   }
   else{
     if (length(ROINames) != max(Output$ROI)){
-      cat('ERROR: ROI name data entered incorrectly, ensure it is done in the form c("name for ROI 1", "name for ROI 2", ...) and that you have put a name for each ROI present. \n')
-      cat("you have", max(Output$ROI), "ROIs that need names", "\n", "\n")
-    }
+      message('ERROR: ROI name data entered incorrectly, ensure it is done in the form c("name for ROI 1", "name for ROI 2", ...) and that you have put a name for each ROI present. \n')
+      message("you entered names for ", length(ROINames), " ROIs but need names for ", max(Output$ROI), " ROIs.", "\n")    }
     else{
       Output$ROIName <- ROINames[Output$ROI]
-      cat("ROI name data added successfully!", "\n", "\n")
+      cat("ROI name data added successfully!", "\n")
     }
   }
   
