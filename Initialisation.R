bb90f78f7db61f7a3f4332c54a49186ce9d339b6
The_Tedstar <edward.aplin@st-annes.ox.ac.uk>
2025-08-15 10:33:51 +0100

    updating renv

--- Initialisation.R
+++ Initialisation.R
@@ -1,2 +1,8 @@
+### Code for initialising the 2T2C project
+## Ensure to run this code before trying to use 2T2C.qmd
+# you should only need to run it for the first time after downloading
+# if issues occur when using 2T2C.qmd then try rerunning this script
+# if that doesn't work then redownload it from the github page and run this script again
+
 renv::restore()
 renv::status()
